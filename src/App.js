195b4c47b2065815c537abd6f5707acf37ab90d2
Aditya-Kulkarni-00 <44834398+Aditya-Kulkarni-00@users.noreply.github.com>
2022-10-13 09:06:31 +0530

    CodeSandbox Editor
    created using npm package @codesandbox/sandpack-react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {Sandpack} from '@codesandbox/sandpack-react'
+function App() {
+  return (
+    <div className="App">
+    <Sandpack template='react'/>
+    </div>
+  );
+}
+
+export default App;
